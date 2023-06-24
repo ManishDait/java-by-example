0aabd3937bd36dec9311b7737e03ed099e3dd3b2
Manish Dait <daitmanish88@gmail.com>
2023-06-24 17:45:46 +0530

    Fix typo mistake.

--- src/test/java/com/javabyexample/algorithms/LinearSearchSnippetTest.java
+++ src/test/java/com/javabyexample/algorithms/LinearSearchSnippetTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 
 public class LinearSearchSnippetTest {
     @Test
-    public void testLinearRegression(){
+    public void testLinearSearch(){
         int arr[] = {1,45,2,31,4,7,45,12,45};
 
         assertEquals(5, LinearSearchSnippet.linearSearch(arr, 7));
