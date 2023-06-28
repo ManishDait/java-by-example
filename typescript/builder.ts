1a0bc4e95a9c7c7912df83b6355893c7264b9ef4
Manish Dait <daitmanish88@gmail.com>
2023-06-28 11:53:59 +0530

    Fix alignment of code.

--- typescript/builder.ts
+++ typescript/builder.ts
@@ -1,3 +1,27 @@
+/*
+ * MIT License
+ * 
+ * Copyright (c) 2023 Manish Dait
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 let code:any = document.getElementById('code');
 let title:any = document.getElementById('title');
 let subtitle:any = document.getElementById('subtitle');
@@ -9,16 +33,13 @@ let path:string;
 
 path = window.location.href.split('#')[1].replace(/%20/g,' ');
 
-function readTextFile(file:string)
-{
+function readTextFile(file:string) {
     var rawFile = new XMLHttpRequest();
     rawFile.open("GET", file, false);
     rawFile.onreadystatechange = function ()
     {
-        if(rawFile.readyState === 4)
-        {
-            if(rawFile.status === 200 || rawFile.status == 0)
-            {
+        if (rawFile.readyState === 4) {
+            if (rawFile.status === 200 || rawFile.status == 0) {
                 var allText:any = rawFile.responseText;
 
                 title!.innerHTML = `Java by Example:`;
@@ -40,29 +61,28 @@ function readTextFile(file:string)
     rawFile.send(null);
 }  
 
-function reload(path:string){
+function reload(path:string) {
     window.location.href = `https://manishdait.github.io/java-by-example/template/snippet.html#${path}`;
     refresh();
 }
-function refresh(){
+function refresh() {
     window.location.reload();
 }
 
 
-function copyText(){
+function copyText() {
     navigator.clipboard.writeText(code.innerText);
     copy.innerHTML = 'done'
 }
 
-function darkMode(){
-    if(localStorage.getItem('mode') == 'dark'){
+function darkMode() {
+    if (localStorage.getItem('mode') == 'dark') {
         localStorage.setItem("mode", "dark");
         dark_toggle!.innerHTML = 'dark_mode'
         document.documentElement.style.setProperty('--bg-color','rgb(27, 27, 27)');
         document.documentElement.style.setProperty('--text-color','rgb(224, 224, 224)');
         document.documentElement.style.setProperty('--primary-color','rgb(65, 63, 63)');
-    }
-    else{
+    } else{
         localStorage.setItem("mode", "light");
         dark_toggle!.innerHTML = 'wb_sunny'
         document.documentElement.style.setProperty('--bg-color','rgb(243, 243, 243)');
@@ -71,15 +91,14 @@ function darkMode(){
     }
 }
 
-function change_darkMode(){
-    if(localStorage.getItem('mode') == 'light'){
+function change_darkMode() {
+    if (localStorage.getItem('mode') == 'light') {
         localStorage.setItem("mode", "dark");
         dark_toggle!.innerHTML = 'dark_mode'
         document.documentElement.style.setProperty('--bg-color','rgb(27, 27, 27)');
         document.documentElement.style.setProperty('--text-color','rgb(224, 224, 224)');
         document.documentElement.style.setProperty('--primary-color','rgb(65, 63, 63)')
-    }
-    else{
+    } else {
         localStorage.setItem("mode", "light");
         dark_toggle!.innerHTML = 'wb_sunny'
         document.documentElement.style.setProperty('--bg-color','rgb(243, 243, 243)');
--- typescript/index.ts
+++ typescript/index.ts
@@ -1,33 +1,50 @@
+/*
+ * MIT License
+ * 
+ * Copyright (c) 2023 Manish Dait
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 let listEle = document.getElementById('list');
 let toggle = document.getElementById('dark_toggle');
+
 let list:any = [];
-function readExampleFile(file:string)
-{
+
+function readExampleFile(file:string) {
     var rawFile = new XMLHttpRequest();
     rawFile.open("GET", file, false);
-    rawFile.onreadystatechange = function ()
-    {
-        if(rawFile.readyState === 4)
-        {
+    rawFile.onreadystatechange = function () {
+        if(rawFile.readyState === 4) {
             if(rawFile.status === 200 || rawFile.status == 0)
             {
                 var allText:any = rawFile.responseText;
                 var titles:string[] =  allText.match(/(?<!#)## \w+/g)
 
-                
-                
-                
-                for (var title of titles){
+                for (var title of titles) {
                     var subtitles:string[]
-                    if(titles.indexOf(title)+1 == titles.length){
+                    if (titles.indexOf(title) + 1 == titles.length) {
                         subtitles = allText.split(title)[1].trim().match(/### [A-Za-z ]+/g)
-                    }
-
-                    else{
-                        var nextTitle = titles[titles.indexOf(title)+1];
+                    } else {
+                        var nextTitle = titles[titles.indexOf(title) + 1];
                         subtitles = allText.split(title)[1].trim().split(nextTitle)[0].trim().match(/### [A-Za-z ]+/g)
                     }
-                    
 
                     title = title.split('##')[1].trim();
                     var li = document.createElement('li');
@@ -35,7 +52,7 @@ function readExampleFile(file:string)
 
                     var ul = document.createElement('ul');
                     ul.style.marginLeft = '1em';
-                    for(var subtitle of subtitles){
+                    for (var subtitle of subtitles) {
                         var l = document.createElement('li');
                         subtitle = subtitle.split('###')[1].trim();
                         l.innerHTML = `<a href="template/snippet.html#${subtitle}">${subtitle}</a>`;
@@ -53,16 +70,14 @@ function readExampleFile(file:string)
     rawFile.send(null);
 }    
 
-function toggleDarkMode(){
-    if(toggle!.innerText == 'wb_sunny' || localStorage.getItem('mode') == 'light'){
+function toggleDarkMode() {
+    if (toggle!.innerText == 'wb_sunny' || localStorage.getItem('mode') == 'light') {
         localStorage.setItem("mode", "dark");
         toggle!.innerHTML = 'dark_mode'
         document.documentElement.style.setProperty('--bg-color','rgb(27, 27, 27)');
         document.documentElement.style.setProperty('--text-color','rgb(224, 224, 224)');
         document.documentElement.style.setProperty('--primary-color','rgb(65, 63, 63)');
-    }
-
-    else{
+    } else {
         localStorage.setItem("mode", "light");
         toggle!.innerHTML = 'wb_sunny'
         document.documentElement.style.setProperty('--bg-color','rgb(243, 243, 243)');
@@ -72,16 +87,14 @@ function toggleDarkMode(){
     }
 }
 
-function checkDarkMode(){
-    if(localStorage.getItem('mode') == 'dark'){
+function checkDarkMode() {
+    if (localStorage.getItem('mode') == 'dark') {
         localStorage.setItem("mode", "dark");
         toggle!.innerHTML = 'dark_mode'
         document.documentElement.style.setProperty('--bg-color','rgb(27, 27, 27)');
         document.documentElement.style.setProperty('--primary-color','rgb(65, 63, 63)')
         document.documentElement.style.setProperty('--text-color','rgb(255, 255, 255)');
-    }
-
-    else{
+    } else {
         localStorage.setItem("mode", "light");
         toggle!.innerHTML = 'wb_sunny'
         document.documentElement.style.setProperty('--bg-color','rgb(243,243,243)');
